8de4dce6f354172aa6cafec67b57a8be9551b659
Krishna Ashish <u1477143@utah.edu>
2024-03-26 14:06:15 -0600

    LAB 06 --- Checkoff02

--- LAB06/Core/Src/main.c
+++ LAB06/Core/Src/main.c
@@ -63,6 +63,7 @@ void SystemClock_Config(void);
 int main(void)
 {
 	uint16_t potValue = 0;
+	const uint8_t sine_table[32] = {127,151,175,197,216,232,244,251,254,251,244,232,216,197,175,151,127,102,78,56,37,21,9,2,0,2,9,21,37,56,78,102};
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
@@ -82,85 +83,23 @@ int main(void)
    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
 	// Enable ADC clock
   RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+		//Enable DAC Clock 
+		RCC->APB1ENR |= RCC_APB1ENR_DACEN;
 	
 	 GPIO_InitTypeDef initc6789 = {GPIO_PIN_8 | GPIO_PIN_9|GPIO_PIN_6|GPIO_PIN_7, GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW,GPIO_NOPULL};
 	 HAL_GPIO_Init(GPIOC, &initc6789);
-   GPIOC->MODER |= GPIO_MODER_MODER0; // Set PC0 as analog mode
-		//set adc 8 bit bit 3&4, continuos conversion mode bit13, hardware trigge disabled - 10,11
-		ADC1->CFGR1 |= ((1<<4) | (1<<3)| (1<<13) | (0<<10) | (0<<11));
+		GPIOA->MODER |= (1<<8) | (1<<9); // Set PA4 as analog mode
+		GPIOA->PUPDR &= ~((1<<8)|(1<<9));
+		DAC->SWTRIGR |=1;
+	 DAC->CR |=1;
+	 uint16_t i =0;
 	
-		ADC1->CHSELR |= (1<<10); //enable input channel 10 for pin 0
-    ADC1->CR |= (0<<0); //clear aden
-		ADC1->CFGR1 |= (0<<0); //clear dmaen
-	//calibration
-		if ((ADC1->CR & ADC_CR_ADEN) != 0) 
-	{
-		ADC1->CR |= ADC_CR_ADDIS; 
-	}
-		while ((ADC1->CR & ADC_CR_ADEN) != 0)
-	{
-	}
-		ADC1->CFGR1 &= ~ADC_CFGR1_DMAEN; 
-		ADC1->CR |= ADC_CR_ADCAL; 
-	
-	//enable adcen
-		while ((ADC1->CR & ADC_CR_ADCAL) != 0) 
-	{
-		GPIOC->ODR |= (1<<6);
-	}
-		if ((ADC1->ISR & ADC_ISR_ADRDY) != 0) 
-		{
-			ADC1->ISR |= ADC_ISR_ADRDY; 
-		}
-		ADC1->CR |= ADC_CR_ADEN;
-		while ((ADC1->ISR & ADC_ISR_ADRDY) == 0)
-		{
-			GPIOC->ODR |= (1<<7);
-		}
-    
-  while (1)
-  {
-		ADC1->CR |= ADC_CR_ADSTART; // Start conversion
-    
-    while (!(ADC1->ISR & ADC_ISR_EOC))
-		{
-		
-			GPIOC->ODR |= (1<<6); 
-		}; 
-    potValue = ADC1->DR;
-		if (potValue >= 10) 
-			{
-            GPIOC->ODR |= (1<<6) ; // Turn on RED LED 
-      } 
-		else 
-			{
-            GPIOC->ODR &= ~(1<<6); // Turn off RED LED 
-      }
-     if (potValue >= 30) 
-			{
-            GPIOC->ODR |= (1<<7); // Turn on Blue LED
-      } 
-			else 
-			{
-            GPIOC->ODR &= ~(1<<7); // Turn off Blue LED
-      }
-      if (potValue >= 40)
-		  {
-            GPIOC->ODR |= (1<<8); // Turn on Orange LED
-      } 
-			else 
-			{
-            GPIOC->ODR &= ~(1<<8); // Turn off Orange LED
-      }
-      if (potValue >= 20) 
-			{
-            GPIOC->ODR |= (1<<9); // Turn on Green LED
-      } 
-			else 
-			{
-            GPIOC->ODR &= ~(1<<9); // Turn off Green LED
-      }
-  }
+	 while(1)
+	 {
+		 if(i ==32)
+			 i=0;
+		 DAC->DHR8R1 = 	sine_table[i];
+HAL_Delay(0);	i++; }
 }
 
 /**
